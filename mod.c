c1d0da87489dbae6050bb70e077d8b1983e4a63c
Philippe Daouadi <philippe@ud2.org>
2022-04-07 09:18:59 +0200

    Fix error handling

--- mod.c
+++ mod.c
@@ -8,30 +8,37 @@ MODULE_AUTHOR("Philippe Daouadi");
 MODULE_DESCRIPTION("Remove kernel lockdown after boot");
 MODULE_VERSION("0.01");
 
-unsigned long (*kla)(const char *name);
+unsigned long (*kla)(const char* name);
 uint8_t* kernel_locked_down;
 uint8_t old_value;
 
 static int resolve_kla(void)
 {
-	struct kprobe kla_kp = { .symbol_name = "kallsyms_lookup_name" };
+  struct kprobe kla_kp = {.symbol_name = "kallsyms_lookup_name"};
 
-	int ret = register_kprobe(&kla_kp);
-	if (ret < 0) {
-		pr_err("resolve_kla: register_kprobe failed (%pe)\n",
-		       ERR_PTR(ret));
-		return ret;
-	}
-	pr_devel("resolved kallsyms_lookup_name");
-	kla = (void *)kla_kp.addr;
-	unregister_kprobe(&kla_kp);
-	return 0;
+  int ret = register_kprobe(&kla_kp);
+  if (ret < 0)
+  {
+    pr_err("unlockdown: register_kprobe failed (%pe)\n", ERR_PTR(ret));
+    return ret;
+  }
+  pr_devel("unlockdown: resolved kallsyms_lookup_name\n");
+  kla = (void*)kla_kp.addr;
+  unregister_kprobe(&kla_kp);
+  return 0;
 }
 
 static int __init mod_init(void)
 {
-  resolve_kla();
+  int err = resolve_kla();
+  if (err < 0)
+    return err;
   kernel_locked_down = (uint8_t*)kla("kernel_locked_down");
+  if (!kernel_locked_down)
+  {
+    printk(KERN_ERR "unlockdown: failed to find kernel_locked_down symbol\n");
+    return -ENOENT;
+  }
   old_value = *kernel_locked_down;
   *kernel_locked_down = 0;
   printk(KERN_INFO "unlockdown: kernel lockdown lifted\n");
